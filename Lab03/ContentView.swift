f3f0176e6c35b25b93843bf7e886fa00a1befb6e
Agsayt <111ag@rambler.ru>
2022-02-20 02:48:47 +0300

    Initial Commit

---
+++ Lab03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab03
+//
+//  Created by Admin on 20.02.2022.
+//  Copyright © 2022 G483. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
